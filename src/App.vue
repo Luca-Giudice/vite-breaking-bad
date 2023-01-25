30a3cb9e51dee97847e156efe559259e0a4987c9
Luca-Giudice <2giudice4@gmail.com>
2023-01-25 16:33:13 +0100

    import-main

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
-<script></script>
-<template></template>
-<style></style>
+<script>
+import AppMain from './components/AppMain.vue';
+export default{
+  components: {AppMain}
+};
+</script>
+
+
+<template>
+
+<header class="text-center pt-5"><h1>Pokedex</h1></header>
+<app-main></app-main>
+</template>
+
+
+
+<style lang="scss">
+  @use './assets/scss/style.scss';
+</style>
